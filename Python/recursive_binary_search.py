e63d184f034885ddd1174615848eef67d76b9c6d
leczy <irabor.alex55@gmail.com>
2021-06-16 11:05:19 +0100

    updated recursive_binary_search.py

--- Python/recursive_binary_search.py
+++ Python/recursive_binary_search.py
@@ -1,6 +1,13 @@
-
+# lst is an array of items
+# target is the value we are looking for
+# start is the starting index
+# end is the ending index
 def recursiveBinarySearch(lst, target, start=0, end=None):
-    if len(lst) == 0 :
+
+    # midpoint = (start + end)//2
+
+    # if the array is empty or the element is not in the array
+    if len(lst) == 0 or start > end:
         return False
 
     if end is None :
@@ -12,15 +19,15 @@ def recursiveBinarySearch(lst, target, start=0, end=None):
         return midpoint
     else:
         if lst[midpoint] < target :
-            recursiveBinarySearch(lst, target, midpoint + 1, end)
+           return recursiveBinarySearch(lst, target, midpoint + 1, end)
 
         else:
-            recursiveBinarySearch(lst, target, start, midpoint - 1)
+            return recursiveBinarySearch(lst, target, start, midpoint - 1)
 
 
 
 elements = [1,2,3,4,5,6,7]
-targetElement = 6
+targetElement = 10
 
 print(recursiveBinarySearch(elements, targetElement))
 
