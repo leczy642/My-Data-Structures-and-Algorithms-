5f0cf76219a3c18a4a75c04d2ddeaebda7f43c52
leczy <leczy642@yahoo.com>
2021-06-29 15:21:46 +0100

    Add files via upload

---
+++ Python/binary_search.js
@@ -0,0 +1,36 @@
+/*
+Binary search locates the position of an element in a sorted array
+by repeatedly dividing in half the portion of the list that can contain the item, until
+it has narrowed down the position to just one.
+Time Complexity : O(Logn)
+Space Complexity : O(1)
+*/
+//lst is an array of items
+//taget is the value we are looking for 
+function binarySearch(lst, target){
+    let first = 0;
+    let last = lst.length - 1; 
+
+    while (first <= last){
+        const mid = (Math.floor((first+last)/2));
+
+        if(lst[mid] === target){
+        return mid;
+        }
+        if(lst[mid] < target){
+            first = mid + 1;
+        }
+        else{
+            last = mid - 1;
+        }
+    }
+    return null;
+}
+
+let items = [1,2,3,4,5,6,7,8];
+let collections = ["Alex Irabor", "Daniel Irabor", "Ehimare Daniel", "Queen", "Victor"];
+let searchString = "Victor";
+let targetElement = 2;
+
+console.log(binarySearch(items, targetElement));
+console.log(binarySearch(collections, searchString));
---
+++ Python/linear_search.js
@@ -0,0 +1,25 @@
+/*
+Linear search starts at the beginning and goes through all the elements in the list
+until the target element is found
+Runtime
+Time Complexity : O(n)
+Space complexity : O(1)
+*/
+//lst is an array of items
+//taget is the value we are looking for 
+function linearSearch(lst, target){
+    for(let i = 0; i < lst.length; i++){
+        if(lst[i] === target){
+            return i;
+        }
+    }
+    return -1;
+}
+  
+let items = [1,2,3,4,5,6,7,8,9];
+let targetElement = 9;
+let collections = ["Alex Irabor", "Daniel Irabor", "Ehimare Daniel", "Queen", "Victor"];
+let searchString = "Victor";
+
+console.log(linearSearch(items, targetElement));
+console.log(linearSearch(collections, searchString));
