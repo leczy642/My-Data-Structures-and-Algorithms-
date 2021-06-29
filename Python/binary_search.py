9d9507157c828d71320aa3e08e00676c34905db4
leczy <irabor.alex55@gmail.com>
2021-06-29 15:00:26 +0100

    updated binary_search.py

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,3 +1,10 @@
+"""
+Binary search locates the position of an element in a sorted array
+by repeatedly dividing in half the portion of the list that can contain the item, until
+it has narrowed down the position to just one.
+Time Complexity : O(Logn)
+Space Complexity : O(1)
+"""
 # lst is an array of items
 # target is the value we are looking for
 
