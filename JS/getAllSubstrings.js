09e2ba604622d169f83203461ac043d8242ff333
leczy <irabor.alex55@gmail.com>
2022-01-25 12:44:13 +0100

    Update getAllSubstrings.js

--- JS/getAllSubstrings.js
+++ JS/getAllSubstrings.js
@@ -2,8 +2,10 @@ function getAllSubstrings(string){
 	const results = []
 	for(let i = 0; i < string.length; i++){
 		for(let j = i + 1; j < string.length + 1; j++){
-			results.push(string.slice(i,j))//['a','ab','aba',..]
+			results.push(string.slice(i,j))
 		}
 	}
 	return results;
 }
+//input : "aba"
+//output : ['a','ab','aba',..]
