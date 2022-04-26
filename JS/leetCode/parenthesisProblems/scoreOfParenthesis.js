7226c3723a33cd6025c93ba8de58817d3cb6b948
leczy <irabor.alex55@gmail.com>
2022-04-26 11:44:28 +0100

    added procedure for solving along with time and space complexity

--- JS/leetCode/parenthesisProblems/scoreOfParenthesis.js
+++ JS/leetCode/parenthesisProblems/scoreOfParenthesis.js
@@ -1,6 +1,21 @@
 //https://www.youtube.com/watch?v=jfmJusJ0qKM
 //score = stack.pop() + Math.max(score * 2, 1);
 //(())
+//https://www.youtube.com/watch?v=jfmJusJ0qKM
+//score = stack.pop() + Math.max(score * 2, 1);
+//(())
+//Time: O(n), Space O(n)
+//procedure
+//1. initialize a stack (called openStack) to store the scores
+//2. initialize a variable (called score) to hold the current score, initially set to 0
+//3. traverse the string
+//   --if the current string is an open parenthesis
+//       --push the current score to the stack
+//       --reset the score value to 0
+//   --else the current string is a closing parenthesis
+//       --calculate the current score (score = max(score * 2, 1) + stack.pop())
+//    break out of the loop
+// return the score
 var scoreOfParentheses = function(s) {
    const openStack = [];
     let score = 0;
