950f11a5fc0e5ba31c5bb96e1d84de7c7ea04f95
leczy <irabor.alex55@gmail.com>
2022-04-26 11:51:27 +0100

    Added procedure to validParenthesisString.js

--- JS/leetCode/parenthesisProblems/validParenthesisString.js
+++ JS/leetCode/parenthesisProblems/validParenthesisString.js
@@ -1,3 +1,23 @@
+//Time O(n), Space O(n)
+//procedure 1 using stacks
+//1. loop through the array
+    //  ---if we find an open parenthesis we push it to the openStack
+    //  ---else if we find a star we push it to the starStack
+    //  ----else (the current element is a closing brace)
+    //          ---if the openStack is not empty, then we pop() the openStack
+    //          --- else if the starStack is not empty, the we pop() the starStack
+    //          --- else we return false because there is no matching opening parenthesis for   //the current closing parenthesis
+    // the loop ends, then we come out of the loop.
+    
+    //2. if we have any remaining opening parenthesis and star parenthesis in the openStack and starStack we need to ensure that the string is a valid parenthsis string
+    //   ---initialize counters for openStack and starStack
+    //   ---while the current openStack < current starStack
+    //       ---pop the openStack
+    //       ---pop the starStack
+    //       ---decrease both counters
+    
+    //3. return true if the openStack.length is 0
+
 var checkValidString = function(s) {
     //also try this test case
     //'(*(**)'
